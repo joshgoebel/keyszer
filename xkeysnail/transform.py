f843f49bda79fafc8f4872bb351e667d5be10584
Josh Goebel <me@joshgoebel.com>
2022-05-26 07:53:55 -0400

    refactor xorg support to sep file

--- xkeysnail/transform.py
+++ xkeysnail/transform.py
@@ -6,41 +6,9 @@ from inspect import signature
 from .key import Action, Combo, Key, Modifier
 from .output import Output 
 from evdev import ecodes
-
+from .xorg import get_active_window_wm_class
 
 __author__ = 'zh'
-
-# ============================================================ #
-
-import Xlib.display
-
-
-def get_active_window_wm_class(display=Xlib.display.Display()):
-    """Get active window's WM_CLASS"""
-    current_window = display.get_input_focus().focus
-    pair = get_class_name(current_window)
-    if pair:
-        # (process name, class name)
-        return str(pair[1])
-    else:
-        return ""
-
-
-def get_class_name(window):
-    """Get window's class name (recursively checks parents)"""
-    try:
-        wmname = window.get_wm_name()
-        wmclass = window.get_wm_class()
-        # workaround for Java app
-        # https://github.com/JetBrains/jdk8u_jdk/blob/master/src/solaris/classes/sun/awt/X11/XFocusProxyWindow.java#L35
-        if (wmclass is None and wmname is None) or "FocusProxy" in wmclass:
-            parent_window = window.query_tree().parent
-            if parent_window:
-                return get_class_name(parent_window)
-            return None
-        return wmclass
-    except:
-        return None
 
 # ============================================================ #
 
---
+++ xkeysnail/xorg.py
@@ -0,0 +1,29 @@
+import Xlib.display
+
+
+def get_active_window_wm_class(display=Xlib.display.Display()):
+    """Get active window's WM_CLASS"""
+    current_window = display.get_input_focus().focus
+    pair = get_class_name(current_window)
+    if pair:
+        # (process name, class name)
+        return str(pair[1])
+    else:
+        return ""
+
+
+def get_class_name(window):
+    """Get window's class name (recursively checks parents)"""
+    try:
+        wmname = window.get_wm_name()
+        wmclass = window.get_wm_class()
+        # workaround for Java app
+        # https://github.com/JetBrains/jdk8u_jdk/blob/master/src/solaris/classes/sun/awt/X11/XFocusProxyWindow.java#L35
+        if (wmclass is None and wmname is None) or "FocusProxy" in wmclass:
+            parent_window = window.query_tree().parent
+            if parent_window:
+                return get_class_name(parent_window)
+            return None
+        return wmclass
+    except:
+        return None
