13575532b21f2af721d1503284cdd6787e50935a
Josh Goebel <me@joshgoebel.com>
2022-06-06 01:37:18 -0400

    fix examples

--- example/config.py
+++ example/config.py
@@ -2,19 +2,22 @@
 timeout(1)
 
 # [Global modemap] Change modifier keys as in xmodmap
-modmap({
+modmap("default", {
     Key.CAPSLOCK: Key.LEFT_CTRL
 })
 
 # [Conditional modmap] Change modifier keys in certain applications
-conditional_modmap(re.compile(r'Emacs'), {
-    Key.RIGHT_CTRL: Key.ESC,
-})
+conditional(
+    lambda ctx: re.compile(r'Emacs').search(ctx.wm_class),
+    modmap("Emacs",{
+        Key.RIGHT_CTRL: Key.ESC,
+    })
+)
 
 # [Multipurpose modmap] Give a key two meanings. A normal key when pressed and
 # released, and a modifier key when held down with another key. See Xcape,
 # Carabiner and caps2esc for ideas and concept.
-multipurpose_modmap(
+multipurpose_modmap("default",
     # Enter is enter when pressed and released. Control when held down.
     {Key.ENTER: [Key.ENTER, Key.RIGHT_CTRL]}
 
@@ -25,111 +28,108 @@ multipurpose_modmap(
 
 # [Conditional multipurpose modmap] Multipurpose modmap in certain conditions,
 # such as for a particular device.
-conditional_multipurpose_modmap(lambda wm_class, device_name: device_name.startswith("Microsoft"), {
-   # Left shift is open paren when pressed and released.
-   # Left shift when held down.
-   Key.LEFT_SHIFT: [Key.KPLEFTPAREN, Key.LEFT_SHIFT],
+conditional(
+    lambda ctx: ctx.device_name.startswith("Microsoft"),
+    multipurpose_modmap("Microsoft keyboard", {
+    # Left shift is open paren when pressed and released.
+    # Left shift when held down.
+    Key.LEFT_SHIFT: [Key.KPLEFTPAREN, Key.LEFT_SHIFT],
 
-   # Right shift is close paren when pressed and released.
-   # Right shift when held down.
-   Key.RIGHT_SHIFT: [Key.KPRIGHTPAREN, Key.RIGHT_SHIFT]
-})
+    # Right shift is close paren when pressed and released.
+    # Right shift when held down.
+    Key.RIGHT_SHIFT: [Key.KPRIGHTPAREN, Key.RIGHT_SHIFT]
+    })
+)
 
 
 # Keybindings for Firefox/Chrome
-keymap(re.compile("Firefox|Google-chrome"), {
-    # Ctrl+Alt+j/k to switch next/previous tab
-    K("C-M-j"): K("C-TAB"),
-    K("C-M-k"): K("C-Shift-TAB"),
-    # Type C-j to focus to the content
-    K("C-j"): K("C-f6"),
-    # very naive "Edit in editor" feature (just an example)
-    K("C-o"): [K("C-a"), K("C-c"), launch(["gedit"]), sleep(0.5), K("C-v")]
-}, "Firefox and Chrome")
+conditional(
+    lambda ctx: re.compile("Firefox|Google-chrome").search(ctx.wm_class),
+    keymap("Firefox and Chrome", {
+        # Ctrl+Alt+j/k to switch next/previous tab
+        K("C-M-j"): K("C-TAB"),
+        K("C-M-k"): K("C-Shift-TAB"),
+        # Type C-j to focus to the content
+        K("C-j"): K("C-f6"),
+        # very naive "Edit in editor" feature (just an example)
+        K("C-o"): [K("C-a"), K("C-c"), launch(["gedit"]), sleep(0.5), K("C-v")]
+    })
+)
 
 # Keybindings for Zeal https://github.com/zealdocs/zeal/
-# when(
-#     lambda window: window.klass != "Emacs", 
-#     lambda device: device == "Apple, Inc Apple Keyboard",
-#     keymap(re.compile("Zeal"), {
-#         # Ctrl+s to focus search area
-#         K("C-s"): K("C-k"),
-#     }, "Zeal")
-# )
-
-keymap("Zeal", 
-    {
+conditional(
+    lambda ctx: re.compile("Zeal").search(ctx.wm_class),
+    keymap("Zeal", {
         # Ctrl+s to focus search area
         K("C-s"): K("C-k"),
-    },
-    [
-        lambda window: window.klass != "Emacs", 
-        lambda device: device == "Apple, Inc Apple Keyboard"
-    ]
+    })
 )
 
 # Emacs-like keybindings in non-Emacs applications
-keymap(lambda wm_class: wm_class not in ("Emacs", "URxvt"), {
-    # Cursor
-    K("C-b"): with_mark(K("left")),
-    K("C-f"): with_mark(K("right")),
-    K("C-p"): with_mark(K("up")),
-    K("C-n"): with_mark(K("down")),
-    K("C-h"): with_mark(K("backspace")),
-    # Forward/Backward word
-    K("M-b"): with_mark(K("C-left")),
-    K("M-f"): with_mark(K("C-right")),
-    # Beginning/End of line
-    K("C-a"): with_mark(K("home")),
-    K("C-e"): with_mark(K("end")),
-    # Page up/down
-    K("M-v"): with_mark(K("page_up")),
-    K("C-v"): with_mark(K("page_down")),
-    # Beginning/End of file
-    K("M-Shift-comma"): with_mark(K("C-home")),
-    K("M-Shift-dot"): with_mark(K("C-end")),
-    # Newline
-    K("C-m"): K("enter"),
-    K("C-j"): K("enter"),
-    K("C-o"): [K("enter"), K("left")],
-    # Copy
-    K("C-w"): [K("C-x"), set_mark(False)],
-    K("M-w"): [K("C-c"), set_mark(False)],
-    K("C-y"): [K("C-v"), set_mark(False)],
-    # Delete
-    K("C-d"): [K("delete"), set_mark(False)],
-    K("M-d"): [K("C-delete"), set_mark(False)],
-    # Kill line
-    K("C-k"): [K("Shift-end"), K("C-x"), set_mark(False)],
-    # Undo
-    K("C-slash"): [K("C-z"), set_mark(False)],
-    K("C-Shift-ro"): K("C-z"),
-    # Mark
-    K("C-space"): set_mark(True),
-    K("C-M-space"): with_or_set_mark(K("C-right")),
-    # Search
-    K("C-s"): K("F3"),
-    K("C-r"): K("Shift-F3"),
-    K("M-Shift-key_5"): K("C-h"),
-    # Cancel
-    K("C-g"): [K("esc"), set_mark(False)],
-    # Escape
-    K("C-q"): escape_next_key,
-    # C-x YYY
-    K("C-x"): {
-        # C-x h (select all)
-        K("h"): [K("C-home"), K("C-a"), set_mark(True)],
-        # C-x C-f (open)
-        K("C-f"): K("C-o"),
-        # C-x C-s (save)
-        K("C-s"): K("C-s"),
-        # C-x k (kill tab)
-        K("k"): K("C-f4"),
-        # C-x C-c (exit)
-        K("C-c"): K("C-q"),
-        # cancel
-        K("C-g"): ignore_key,
-        # C-x u (undo)
-        K("u"): [K("C-z"), set_mark(False)],
-    }
-}, "Emacs-like keys")
+conditional(
+    lambda ctx: ctx.wm_class not in ("Emacs", "URxvt"),
+    keymap("Emacs-like keys", {
+        # Cursor
+        K("C-b"): with_mark(K("left")),
+        K("C-f"): with_mark(K("right")),
+        K("C-p"): with_mark(K("up")),
+        K("C-n"): with_mark(K("down")),
+        K("C-h"): with_mark(K("backspace")),
+        # Forward/Backward word
+        K("M-b"): with_mark(K("C-left")),
+        K("M-f"): with_mark(K("C-right")),
+        # Beginning/End of line
+        K("C-a"): with_mark(K("home")),
+        K("C-e"): with_mark(K("end")),
+        # Page up/down
+        K("M-v"): with_mark(K("page_up")),
+        K("C-v"): with_mark(K("page_down")),
+        # Beginning/End of file
+        K("M-Shift-comma"): with_mark(K("C-home")),
+        K("M-Shift-dot"): with_mark(K("C-end")),
+        # Newline
+        K("C-m"): K("enter"),
+        K("C-j"): K("enter"),
+        K("C-o"): [K("enter"), K("left")],
+        # Copy
+        K("C-w"): [K("C-x"), set_mark(False)],
+        K("M-w"): [K("C-c"), set_mark(False)],
+        K("C-y"): [K("C-v"), set_mark(False)],
+        # Delete
+        K("C-d"): [K("delete"), set_mark(False)],
+        K("M-d"): [K("C-delete"), set_mark(False)],
+        # Kill line
+        K("C-k"): [K("Shift-end"), K("C-x"), set_mark(False)],
+        # Undo
+        K("C-slash"): [K("C-z"), set_mark(False)],
+        K("C-Shift-ro"): K("C-z"),
+        # Mark
+        K("C-space"): set_mark(True),
+        K("C-M-space"): with_or_set_mark(K("C-right")),
+        # Search
+        K("C-s"): K("F3"),
+        K("C-r"): K("Shift-F3"),
+        K("M-Shift-key_5"): K("C-h"),
+        # Cancel
+        K("C-g"): [K("esc"), set_mark(False)],
+        # Escape
+        K("C-q"): escape_next_key,
+        # C-x YYY
+        K("C-x"): {
+            # C-x h (select all)
+            K("h"): [K("C-home"), K("C-a"), set_mark(True)],
+            # C-x C-f (open)
+            K("C-f"): K("C-o"),
+            # C-x C-s (save)
+            K("C-s"): K("C-s"),
+            # C-x k (kill tab)
+            K("k"): K("C-f4"),
+            # C-x C-c (exit)
+            K("C-c"): K("C-q"),
+            # cancel
+            K("C-g"): ignore_key,
+            # C-x u (undo)
+            K("u"): [K("C-z"), set_mark(False)],
+        }
+    })
+)
