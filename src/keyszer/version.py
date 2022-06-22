907a38bdaa8d69631cb1be0e595deabeab2ae262
Josh Goebel <me@joshgoebel.com>
2022-06-22 10:29:49 -0400

    update description to match README

--- src/keyszer/version.py
+++ src/keyszer/version.py
@@ -5,15 +5,17 @@ __version__ = "0.6.0"
 __description__ = "A smart, flexible key remapper for Linux/X11."
 
 __doc__ = """
-``keyszer`` is a smart and flexible key remapper for Linux/X11t.
-It's like ``xmodmap`` but better.
+``keyszer`` is a smart and flexible key remapper for Linux/X11.
+Think ``xmodmap``, but new and improved!.
 
--  Has high-level and flexible remapping mechanisms, such as
-
-   -  **per-application keybindings can be defined**
-   -  **multiple stroke keybindings can be defined** such as
-      ``Ctrl+x Ctrl+c`` to ``Ctrl+q``
-
--  Runs in low-level layer (``evdev`` and ``uinput``), making
-   **remapping work in almost all the places**
+- Low-level library usage (`evdev` and `uinput`) allows remapping to work from
+  the console all the way into X11.
+- High-level and incredibly flexible remapping mechanisms:
+    - _per-application keybindings_ - bindings that change depending on the
+      active X11 application or window
+    - _multiple stroke keybindings_ - `Ctrl+x Ctrl+c` could map to `Ctrl+q`
+    - _very flexible output_ - `Ctrl-s` can type `:save` then hit enter
+    - _stateful key combos_ - build Emacs style combos with shift/mark
+    - _multipurpose bindings_ - a regular key can become a modifier when held
+    - _arbitrary functions_ - a key combo can run custom Python function
 """
