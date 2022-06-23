6545fca6d34a80e732c9a966fcbc0091ab04e6b8
drjaska <drjaska83@gmail.com>
2022-06-23 20:30:50 +0300

    typo fix in example config.py

--- example/config.py
+++ example/config.py
@@ -22,7 +22,7 @@ multipurpose_modmap("default",
     {Key.ENTER: [Key.ENTER, Key.RIGHT_CTRL]}
 
     # Capslock is escape when pressed and released. Control when held down.
-    # {Key.CAPSLOCK: [Key.ESC, Key.LEFT_CTRL]
+    # {Key.CAPSLOCK: [Key.ESC, Key.LEFT_CTRL]}
     # To use this example, you can't remap capslock with modmap.
 )
 
