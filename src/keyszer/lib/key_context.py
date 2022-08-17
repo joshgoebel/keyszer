5633f3e1258d64973c62e65d082ccc813a9d0432
RedBearAK <64876997+RedBearAK@users.noreply.github.com>
2022-08-17 03:54:09 -0800

    Solution for device.name, passing only device

--- src/keyszer/lib/key_context.py
+++ src/keyszer/lib/key_context.py
@@ -2,27 +2,21 @@ from ..xorg import get_xorg_context
 
 
 class KeyContext:
-    def __init__(self, device_name, device):
-        self._device_name = device_name
+    def __init__(self, device):
         self._X_ctx = None
+        leds_list = []
 
         # Must declare these here or app will crash if keyboard device hasn't been "grabbed" yet
         self._capslock_state = ""
         self._numlock_state = ""
 
-        leds_list = []
-
         # Check for actual device name being present before using evdev's ".leds()" method
+        # or device.name (doesn't like strings)
         if not device == "":
+            self._device_name = device.name
             leds_list = device.leds()
-            if 1 in leds_list:
-                self._capslock_state = "ON"
-            else:
-                self._capslock_state = "OFF"
-            if 0 in leds_list:
-                self._numlock_state = "ON"
-            else:
-                self._numlock_state = "OFF"
+            self._capslock_state = "ON" if 1 in leds_list else "OFF"
+            self._numlock_state = "ON" if 0 in leds_list else "OFF"
 
     def _query_window_context(self):
         # cache this,  think it might be expensive
