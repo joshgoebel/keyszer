2aa3afd5849135de57c1c5be5f45b3b52ef34508
RedBearAK <64876997+RedBearAK@users.noreply.github.com>
2022-08-17 01:48:40 -0800

    Support capslock_state and numlock_state

--- src/keyszer/lib/key_context.py
+++ src/keyszer/lib/key_context.py
@@ -6,6 +6,24 @@ class KeyContext:
         self._device_name = device_name
         self._X_ctx = None
 
+        # Must declare these here or app will crash if keyboard device hasn't been "grabbed" yet
+        self._capslock_state = ""
+        self._numlock_state = ""
+
+        leds_list = []
+
+        # Check for actual device name being present before using evdev's ".leds()" method
+        if not device == "":
+            leds_list = device.leds()
+            if 1 in leds_list:
+                self._capslock_state = "ON"
+            else:
+                self._capslock_state = "OFF"
+            if 0 in leds_list:
+                self._numlock_state = "ON"
+            else:
+                self._numlock_state = "OFF"
+
     def _query_window_context(self):
         # cache this,  think it might be expensive
         if self._X_ctx is None:
@@ -29,3 +47,11 @@ class KeyContext:
     @property
     def device_name(self):
         return self._device_name
+
+    @property
+    def capslock_state(self):
+        return self._capslock_state
+
+    @property
+    def numlock_state(self):
+        return self._numlock_state
