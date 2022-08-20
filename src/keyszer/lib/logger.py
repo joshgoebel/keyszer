547a62791e2f49847226657429a03d5b2d627b7e
RedBearAK <64876997+RedBearAK@users.noreply.github.com>
2022-08-19 17:11:44 -0800

    FLUSH constant to support log flushing argument

--- src/keyszer/lib/logger.py
+++ src/keyszer/lib/logger.py
@@ -1,5 +1,5 @@
 VERBOSE = False
-
+FLUSH = False
 
 def debug(*args, ctx="DD"):
     if not VERBOSE:
@@ -7,21 +7,21 @@ def debug(*args, ctx="DD"):
 
     # allow blank lines without context
     if len(args) == 0 or (len(args) == 1 and args[0] == ""):
-        print("", flush=True)
+        print("", flush=FLUSH)
         return
-    print(f"({ctx})", *args, flush=True)
+    print(f"({ctx})", *args, flush=FLUSH)
 
 
 def warn(*args, ctx="WW"):
-    print(f"({ctx})", *args, flush=True)
+    print(f"({ctx})", *args, flush=FLUSH)
 
 
 def error(*args, ctx="EE"):
-    print(f"({ctx})", *args, flush=True)
+    print(f"({ctx})", *args, flush=FLUSH)
 
 
 def log(*args, ctx="--"):
-    print(f"({ctx})", *args, flush=True)
+    print(f"({ctx})", *args, flush=FLUSH)
 
 
 def info(*args, ctx="--"):
