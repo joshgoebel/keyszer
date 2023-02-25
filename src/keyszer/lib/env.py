91fd8d0c1f4e730960ffcb2915a301d9743110da
RedBearAK <64876997+RedBearAK@users.noreply.github.com>
2023-02-25 05:16:38 -0900

    Update env.py

--- src/keyszer/lib/env.py
+++ src/keyszer/lib/env.py
@@ -8,33 +8,28 @@ from ..config_api import _ENVIRONMENT
 
 # ─── GLOBALS ─────────────────────────────────────────────────────────────────
 
-DISTRO_NAME     = ""
-OS_DISTRO_NAME  = None
-LSB_DISTRO_NAME = None
+DISTRO_NAME         = None
+_os_distro_name     = None
+_lsb_distro_name    = None
 
-SESSION_TYPE    = environ.get("XDG_SESSION_TYPE") or ""
-DESKTOP_ENV     = ""
-_desktop_env    = environ.get("XDG_SESSION_DESKTOP") or environ.get("XDG_CURRENT_DESKTOP")
+SESSION_TYPE        = None
 
-
-def get_env():
-    return {
-                "DISTRO_NAME"   : DISTRO_NAME,
-                "SESSION_TYPE"  : SESSION_TYPE,
-                "DESKTOP_ENV"   : DESKTOP_ENV,
-            }
+DESKTOP_ENV         = None
+_desktop_env        = None
 
 
 # ─── ENVIRONMENT ─────────────────────────────────────────────────────────────────
 
 
-####################################
+########################################################################
 ##  Get distro name
+##  We don't provide a "simplified" distro name
+##  Just pull it from /etc/os-release or /etc/lsb-release
 try:
     with open('/etc/os-release', 'r') as f:
         for line in f:
             if line.startswith('NAME='):
-                OS_DISTRO_NAME = line.split('=')[1].strip().strip('"')
+                _os_distro_name = line.split('=')[1].strip().strip('"')
                 break
 except FileNotFoundError as file_error:
     error(file_error)
@@ -43,22 +38,24 @@ try:
         with open('/etc/lsb-release', 'r') as f:
             for line in f:
                 if line.startswith('DISTRIB_ID='):
-                    LSB_DISTRO_NAME = line.split('=')[1].strip().strip('"')
+                    _lsb_distro_name = line.split('=')[1].strip().strip('"')
                     break
 except FileNotFoundError as file_error:
     error(file_error)
 
-if not OS_DISTRO_NAME and not LSB_DISTRO_NAME:
+if not _os_distro_name and not _lsb_distro_name:
         DISTRO_NAME = 'Unidentified'
         error(f"ENV: Distro name couldn't be found in /etc/os-release or /etc/lsb-release.")
 else:
-    DISTRO_NAME = OS_DISTRO_NAME or LSB_DISTRO_NAME
+    DISTRO_NAME = _os_distro_name or _lsb_distro_name
 
 
-####################################
+########################################################################
 ##  Get session type
+SESSION_TYPE = environ.get("XDG_SESSION_TYPE") or None
+
 if not SESSION_TYPE:  # Why isn't XDG_SESSION_TYPE set? This shouldn't happen.
-    debug(f'ENV: XDG_SESSION_TYPE should really be set. Are you in a graphical environment?')
+    error(f'ENV: XDG_SESSION_TYPE should really be set. Are you in a graphical environment?')
     SESSION_TYPE = environ.get("WAYLAND_DISPLAY")
     if not SESSION_TYPE:
         raise EnvironmentError(
@@ -66,14 +63,13 @@ if not SESSION_TYPE:  # Why isn't XDG_SESSION_TYPE set? This shouldn't happen.
 if SESSION_TYPE.casefold() not in ['x11', 'xorg', 'wayland']:
     raise EnvironmentError(f'\n\nENV: Unknown session type: {SESSION_TYPE}.\n')
 
-if not _desktop_env:
-    _desktop_env = None
-    debug("ENV ERROR: Desktop Environment not found in XDG_SESSION_DESKTOP or XDG_CURRENT_DESKTOP.")
-    debug("ENV ERROR: Config file will not be able to adapt automatically to Desktop Environment.")
 
-
-####################################
+########################################################################
 ##  Get desktop environment
+_desktop_env = environ.get("XDG_SESSION_DESKTOP") or environ.get("XDG_CURRENT_DESKTOP") or None
+
+if not _desktop_env:
+    error("ENV: Desktop Environment not found in XDG_SESSION_DESKTOP or XDG_CURRENT_DESKTOP.")
 
 # Produce a simplified desktop environment name
 de_names = {
@@ -82,7 +78,6 @@ de_names = {
     'Deepin':                   'deepin',
     'Enlightenment':            'enlightenment',
     'GNOME':                    'gnome',
-    'Hyprland':                 'hypr',
     'i3':                       'i3',
     'IceWM':                    'icewm',
     'KDE':                      'kde',
@@ -96,16 +91,24 @@ de_names = {
     'Unity':                    'unity',
     'Xfce':                     'xfce',
 }
-for k, v in de_names.items():
-    # debug(f'{k = :<10} {v = :<10}')
-    if re.search(_desktop_env, k, re.I):
-        DESKTOP_ENV = v
+
+if _desktop_env:
+    for k, v in de_names.items():
+        # debug(f'{k = :<18} {v = :<10}')
+        if re.search(k, _desktop_env, re.I):
+            DESKTOP_ENV = v
+            break
 if not DESKTOP_ENV:
-    DESKTOP_ENV = _desktop_env
+    DESKTOP_ENV = _desktop_env or "Unidentified"
     error(f'ENV: Desktop Environment not in de_names list! Should fix this.\n\t{_desktop_env = }')
 
-# Doublecheck the desktop env by checking for running processes
+# Doublecheck the desktop environment by checking for identifiable running processes
 for proc in psutil.process_iter(['name']):
+    if proc.info['name'] in ['plasmashell', 'kwin_ft', 'kwin_x11']:
+        if DESKTOP_ENV != 'kde':
+            error(f'Desktop may have been misidentified: {DESKTOP_ENV = }. KWin detected.')
+        DESKTOP_ENV = 'kde'
+        break
     if proc.info['name'] == 'gnome-shell':
         if DESKTOP_ENV != 'gnome':
             error(f'Desktop may have been misidentified: {DESKTOP_ENV = }. GNOME Shell detected.')
@@ -116,16 +119,16 @@ for proc in psutil.process_iter(['name']):
             error(f'Desktop may have been misidentified: {DESKTOP_ENV = }. SwayWM detected.')
         DESKTOP_ENV = 'sway'
         break
-    if proc.info['name'] in ['plasmashell', 'kwin_ft', 'kwin_x11']:
-        if DESKTOP_ENV != 'kde':
-            error(f'Desktop may have been misidentified: {DESKTOP_ENV = }. KWin detected.')
-        DESKTOP_ENV = 'kde'
-        break
 
-from distro import name as dname
 
-print(f'## ### testing distro module: {dname() = }')
 debug("")
 debug(f'ENV: {DISTRO_NAME  = }')
 debug(f'ENV: {SESSION_TYPE = }')
 debug(f'ENV: {DESKTOP_ENV  = }')
+
+def get_env():
+    return {
+                "DISTRO_NAME"   : DISTRO_NAME,
+                "SESSION_TYPE"  : SESSION_TYPE,
+                "DESKTOP_ENV"   : DESKTOP_ENV,
+            }
